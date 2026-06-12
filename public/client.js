d9fc1ecb22f87953db39ccdb5b853033489f7d68
Nikolaus Knop <niko.knop003@gmail.com>
2026-06-13 01:16:51 +0200

    Initial commit

---
+++ public/client.js
@@ -0,0 +1,99 @@
+
+let ws;
+
+const btn_connect = document.getElementById("btn-connect")
+
+const controls = document.getElementById("controls")
+const measureNumber = document.getElementById("measure-number")
+const beatsPerBar = document.getElementById("beats-per-bar")
+const currentBeat = document.getElementById("current-beat")
+const toggle_icon = document.getElementById("toggle-icon")
+const position_info = document.getElementById("status-bar")
+const btn_toggle = document.getElementById("btn-toggle")
+const btn_backward = document.getElementById("btn-backward")
+const btn_forward = document.getElementById("btn-forward")
+const waiting_message = document.getElementById("waiting-message")
+
+async function connect() {
+    const protocol = location.protocol === "https:" ? "wss:" : "ws:";
+    ws = new WebSocket(`${protocol}//${location.host}`);
+
+    ws.onopen = () => {
+        console.log("Connected!");
+        btn_connect.style.display = "none";
+        waiting_message.classList.remove("hidden");   
+        toggle_icon.innerHTML = "play_arrow";
+        send_command("i-am-controller");
+    }
+
+    ws.onmessage = (msg) => {
+        const data = JSON.parse(msg.data)
+
+        console.log(data.type)
+        
+        if (data.type === "ponticello-connected") {
+            waiting_message.classList.add("hidden")
+            controls.classList.remove("hidden")
+        }
+
+        if (data.type === "ponticello-disconnected") {
+            waiting_message.classList.remove("hidden")
+            controls.classList.add("hidden")
+            position_info.classList.add("hidden")
+        }
+        
+        if (data.type === "started") {
+            console.log("Started")
+            position_info.classList.remove("hidden")
+            btn_forward.classList.remove("hidden")
+            btn_backward.classList.remove("hidden")
+            toggle_icon.innerHTML = "stop"
+        }
+        
+        if (data.type === "stopped") {
+            console.log("Paused")
+            toggle_icon.innerHTML = "play_arrow"
+            measureNumber.innerHTML = "0"
+            beatsPerBar.innerHTML = "?"
+            currentBeat.innerHTML = "?"
+            position_info.classList.add("hidden");
+            btn_backward.classList.add("hidden");
+            btn_forward.classList.add("hidden")
+        }
+        if (data.type === "beat") {
+            measureNumber.innerHTML = data.measureNumber
+            beatsPerBar.innerHTML = data.beatsPerBar
+            currentBeat.innerHTML = data.currentBeat
+            flashBeatBackground()
+        }
+    }
+}
+
+async function send_command(cmd) {
+    const msg = JSON.stringify({type: cmd});
+    ws.send(msg);
+}
+
+async function forward() {
+    send_command("forward")
+}
+
+async function backward() {
+    send_command("backward")
+}
+
+async function toggle_play() {
+    send_command("toggle")
+}
+
+function flashBeatBackground() {
+
+  const app = document.getElementById("app");
+
+  app.classList.remove("beat-flash");
+
+  // restart animation
+  void app.offsetWidth;
+
+  app.classList.add("beat-flash");
+}
---
+++ server.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const os = require("os")
+
+const app = express();
+app.use(express.static("public"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let ponticello;
+let controllers = new Set();
+
+const commands = ["forward", "backward", "toggle"]
+const updates = ["beat", "started", "playing", "stopped"]
+
+let playing = false;
+
+wss.on("connection", (ws) => {
+    ws.on("message", (msg) => {
+        const data = JSON.parse(msg);
+
+        if (data.type === "i-am-ponticello") {
+            ponticello = ws;
+            console.log("Connected Ponticello, notifying ", controllers.size, " controllers");
+            controllers.forEach((ctrl) => { 
+                ctrl.send(JSON.stringify({type:"ponticello-connected"}))
+            })
+            ws.on("close", () => { 
+                ponticello = null;
+                console.log("Closed Ponticello connection, notifying ", controllers.size, " controllers")
+                controllers.forEach((ctrl) => ctrl.send(JSON.stringify({type:"ponticello-disconnected"})))
+            }) 
+        }
+
+        if (data.type === "i-am-controller") {
+            controllers.add(ws);
+            if (playing) {
+                ws.send(JSON.stringify({type:"started"}));
+                ws.send(JSON.stringify({type:"playing"}))
+            }
+            if (ponticello) {
+                ws.send(JSON.stringify({type: "ponticello-connected"}))
+            }
+            ws.on("close", () => { controllers.delete(ws) });
+            console.log("Connected Controller");
+        }
+
+        if (commands.includes(data.type)) {
+            if (data.type === "toggle") {
+                if (playing) { data.type = "stop" }
+                else { data.type = "start" }
+            }
+            if (ponticello) { 
+                console.log(data);
+                ponticello.send(JSON.stringify(data)); 
+            }
+        }
+
+        if (updates.includes(data.type)) {
+            if (data.type === "started") {
+                playing = true;
+            }
+            if (data.type === "stopped") {
+                playing = false;
+            }
+            controllers.forEach((ctrl) => {
+                console.log(data); 
+                ctrl.send(JSON.stringify(data));
+            })
+        }
+    });
+});
+
+function getLocalIP() {
+  const nets = os.networkInterfaces();
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+
+      if (
+        net.family === "IPv4" &&
+        !net.internal &&
+        !name.includes("Virtual") &&
+        !name.includes("Docker")
+      ) {
+        return net.address;
+      }
+    }
+  }
+}
+
+const port = process.env.PORT || 8080;
+server.listen(port, "0.0.0.0", () => {
+    const ip = getLocalIP();
+    const url = `http://${ip}:${port}`;
+    console.log("Client Adresse: ", url);
+});
